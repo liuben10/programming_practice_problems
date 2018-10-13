2325c3e965da10114d6eb51bd0c675530490436c
liuben10 <liuben10@gmail.com>
2018-10-12 19:13:00 -0700

    Edge case

--- two_sum.py
+++ two_sum.py
@@ -7,12 +7,12 @@ def two_sum(nums, target):
     res = []
 
     for i, num in enumerate(nums):
-        if (target - num in diffs):
+        if (target - num in diffs and diffs[target-num] is not i):
             res.append(i)
             res.append(diffs[target - num])
             return res
 
     return res
 
-print(two_sum([2, 3, 5, 9], 7))
+print(two_sum([3, 2, 4], 6))
         
